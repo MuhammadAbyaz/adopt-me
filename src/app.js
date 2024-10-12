41d1c286b99c6ec8f30c6f0206d7a11defc6be7d
MuhammadAbyaz <m.abyaz681@gmail.com>
2024-10-12 23:53:18 +0500

    init: initialize react app (purana tareeqa 🤌🏻)

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Tommy",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Mano Billi",
+      breed: "Karachi wali",
+    }),
+    React.createElement(Pet, {
+      animal: "Rat",
+      name: "Jerry",
+      breed: "Gutter wala",
+    }),
+  ]);
+};
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
